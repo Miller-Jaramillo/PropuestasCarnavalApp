0485293d9e463e0df7ce68e01a6391a0b3a614ef
Miller Jaramillo <millerjaramillo.mj@gmail.com>
2023-09-01 00:04:26 -0500

    se elimino boton de informacion en prouestas participante

--- resources/views/livewire/propuestas-participante.blade.php
+++ resources/views/livewire/propuestas-participante.blade.php
@@ -12,22 +12,6 @@
             <div class="flex justify-center mt-1 pb-3">
 
 
-                <div>
-                    <x-button wire:click=""
-                        class="inline-flex items-center px-4 py-2
-                        hover:bg-gray-700 dark:hover:bg-gray-400
-                        bg-white dark:bg-gray-800 dark:sm:bg-gray-800 shadow-sm  mr-3  icon-red"
-                        title="Informacion">
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"
-                            class="w-6 h-6   sm:w-6 sm:h-6">
-                            <path fill-rule="evenodd"
-                                d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zm8.706-1.442c1.146-.573 2.437.463 2.126 1.706l-.709 2.836.042-.02a.75.75 0 01.67 1.34l-.04.022c-1.147.573-2.438-.463-2.127-1.706l.71-2.836-.042.02a.75.75 0 11-.671-1.34l.041-.022zM12 9a.75.75 0 100-1.5.75.75 0 000 1.5z"
-                                clip-rule="evenodd" />
-                        </svg>
-                        {{-- <p class="sm:hidden block "> Informacion <br> importante</p> --}}
-                    </x-button>
-
-                </div>
 
                 <div>
                     <x-button wire:click="openFormNuevaPropuesta"
